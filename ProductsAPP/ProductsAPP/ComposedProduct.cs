3e84df73ab19b79bb01796952639f4d91826923c
David Perez <perezdavid2236@gmail.com>
2022-03-01 21:13:09 -0500

    Create other classes withput correct prop types

---
+++ ProductsAPP/ProductsAPP/ComposedProduct.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductsAPP
+{
+    public class ComposedProduct : Product
+    {
+        public decimal Discount { get; set; }
+        public int Products { get; set; }
+
+        public override decimal valueToPay()
+        {
+            return 77;
+        }
+    }
+}
---
+++ ProductsAPP/ProductsAPP/VariablePriceProduct.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductsAPP
+{
+    public class VariablePriceProduct : Product
+    {
+        public float Meassurement { get; set; }
+        public int Quantity { get; set; }
+
+        public override decimal valueToPay()
+        {
+            return 52;
+        }
+    }
+}
