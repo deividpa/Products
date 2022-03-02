0195adbc53b2931298e0334da08be42bf357f81b
David Perez <perezdavid2236@gmail.com>
2022-03-01 20:05:53 -0500

    Create class Product

---
+++ ProductsAPP/ProductsAPP/FixedPriceProduct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductsAPP
+{
+    public class FixedPriceProduct : Product
+    {
+        public override decimal valueToPay()
+        {
+            return Price;
+        }
+
+        public override string ToString()
+        {
+            return $"Price value: {valueToPay}";
+        }
+    }
+}
---
+++ ProductsAPP/ProductsAPP/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductsAPP
+{
+    public abstract class Product
+    {
+        public string Description { get; set; }
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public decimal Tax { get; set; }
+
+        public abstract decimal valueToPay();
+        public override string ToString()
+        {
+            return $"{Description}";
+        }
+
+    }
+}
