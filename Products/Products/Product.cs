6928fa3f1313d742547d0bbe14a216706cafcb05
David Perez <perezdavid2236@gmail.com>
2022-03-01 19:36:23 -0500

    Start

---
+++ Products/Products/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Products {
+
+public abstract class Product
+{
+
+	public string Description { get; set; };
+	public int Id { get; set; };
+	public decimal Price { get; set; };
+	public decimal Tax { get; set; };
+
+	
+
+}
+
