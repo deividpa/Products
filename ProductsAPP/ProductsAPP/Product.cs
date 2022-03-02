b17a4185a5e2b0f4828beae9eb42cabec65832ce
David Perez <perezdavid2236@gmail.com>
2022-03-01 23:44:37 -0500

    Interface Implement

--- ProductsAPP/ProductsAPP/Product.cs
+++ ProductsAPP/ProductsAPP/Product.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ProductsAPP
 {
-    public abstract class Product
+    public abstract class Product : IPay
     {
         public string Description { get; set; }
         public int Id { get; set; }
