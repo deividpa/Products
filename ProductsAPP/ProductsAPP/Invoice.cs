cc0e6fa429b4c707827dd3fe82229f73db0e5d4d
David Perez <perezdavid2236@gmail.com>
2022-03-02 20:05:21 -0500

    invoice Class UPDATE

--- ProductsAPP/ProductsAPP/Invoice.cs
+++ ProductsAPP/ProductsAPP/Invoice.cs
@@ -14,3 +14,43 @@ namespace ProductsAPP
         public addProduct()
     }
 }*/
+
+namespace ProductsAPP
+{
+    public class Invoice : IPay
+    {
+        private List<Product> _productItems;
+
+        public Invoice()
+        {
+            _productItems = new List<Product>();
+        }
+        public Product AddProduct(Product product)
+        {
+            _productItems.Add(product);
+            return product;
+        }
+        public decimal valueToPay()
+        {
+            decimal total = 0;
+            foreach (Product product in _productItems)
+            {
+                total += product.valueToPay();
+            }
+            return total;
+        }
+        public override string ToString()
+        {
+            string productsInfo = "";
+            foreach (Product product in _productItems)
+            {
+                productsInfo += product.ToString() + "\n";
+            }
+            return $"RECEIPT \n" +
+                $"-------------------------------------------------- \n" +
+                $"{productsInfo}" +
+                $"                    ============= \n" +
+                $"TOTAL: {$"              {valueToPay():C2}",15}";
+        }
+    }
+}
